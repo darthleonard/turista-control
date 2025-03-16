c28576b46a3841a1f380c1250359b1da475a4f8f
darthleonard <leolinuxmx@gmail.com>
2025-03-15 22:02:15 -0600

    adding app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.darthleonard.turista-control',
+  appName: 'turista-control',
+  webDir: 'www'
+};
+
+export default config;
