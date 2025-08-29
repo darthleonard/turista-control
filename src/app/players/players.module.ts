1309196b526c8dd270ec96c4cee944b4dc89438f
darthleonard <leolinuxmx@gmail.com>
2025-08-29 13:53:39 -0600

    translate players list

--- src/app/players/players.module.ts
+++ src/app/players/players.module.ts
@@ -1,11 +1,9 @@
 import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { TranslateModule } from '@ngx-translate/core';
 import { IonicModule } from '@ionic/angular';
-
 import { PlayerPageRoutingModule } from './players-routing.module';
-
 import { PlayersListPage } from './players-list/players-list.page';
 import { ComponentsModule } from '../components/components.module';
 
@@ -16,6 +14,7 @@ import { ComponentsModule } from '../components/components.module';
     IonicModule,
     ComponentsModule,
     PlayerPageRoutingModule,
+    TranslateModule,
   ],
   declarations: [PlayersListPage],
 })
